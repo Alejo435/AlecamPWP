c9f50a37ad8d019184072baa3c78f6adc4a20d0d
pl198793 <pl198793@ahschool.com>
2023-02-24 08:19:10 -0500

    hi

--- cam.py
+++ cam.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('/Users/pl198793/MidtermProject/alecam/image/circle-41667.png')
+img = cv2.imread('/Users/pl198793/MidtermProject/alecam/image/Circle-41667.png')
 
 #coverts image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
