a0558fb06a29b225520c30a5ac77b5a80216784e
pl198793 <pl198793@ahschool.com>
2023-02-27 08:11:10 -0500

    hi

--- cam.py
+++ cam.py
@@ -6,3 +6,15 @@ img = cv2.imread('/Users/pl198793/MidtermProject/alecam/alecam/image/Round-Circl
 
 #coverts image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+output = img.copy()
+circle = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1.3,100)
+
+if circle is not None:
+    circle = np.round(circle[0, :]).astype("int")
+    print(circle)
+    for (x, y, r) in circle:
+        cv2.circle(output, (x,y), r, (0, 255, 0), 2)
+
+cv2.imshow("circle",output)
+cv2.waitkey(0)
