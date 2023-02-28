3e8b032ebb36bc255632a778bc1f18235a2d8bb5
pl198793 <pl198793@ahschool.com>
2023-02-28 07:54:13 -0500

    hi

--- cam.py
+++ cam.py
@@ -1,21 +1,21 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 
-img = cv2.imread('IMG-1179.mp4')
+img = cv2.VideoCapture('IMG-1179.mp4')
 
 #coverts image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray_blur = cv2.blur(gray, (3, 3))
 
-# output = img.copy()
-# circle = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 100000), param1 = 50, param2 = 30, minRadius = 100, maxRadius = 300
-#
-# if circle is not None:
-#     circle = np.round(circle[0, :]).astype("int")
-#     print(circle)
-#     for (x, y, r) in circle:
-#         cv2.circle(output, (x,y), r, (0, 255, 0), 2)
-#
-# cv2.imshow("circle",output)
-# cv2.waitkey(0)
+output = img.copy()
+circle = cv2.HoughCircles(gary_blur, cv2.HOUGH_GRADIENT, 1, 100000), param1 = 50, param2 = 30, minRadius = 100, maxRadius = 300
+
+if circle is not None:
+    circle = np.round(circle[0, :]).astype("int")
+    print(circle)
+    for (x, y, r) in circle:
+        cv2.circle(output, (x,y), r, (0, 255, 0), 2)
+
+cv2.imshow("circle",output)
+cv2.waitkey(0)
