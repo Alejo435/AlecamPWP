ef88ddeb5162ea4d84b9987445eb060c89a04329
pl198793 <pl198793@ahschool.com>
2023-03-06 08:11:09 -0500

    hi

--- laslinias.py
+++ laslinias.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('IMG-1201.jpg')
+img = cv2.imread('IMG-1199.jpg')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 50, 150, apertureSize=3)
