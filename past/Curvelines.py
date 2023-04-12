be3b0c91d7edc21f699d7b32eecdc5cee6a26d26
pl198793 <pl198793@ahschool.com>
2023-04-12 07:58:08 -0400

    hi

--- past/Curvelines.py
+++ past/Curvelines.py
@@ -9,7 +9,9 @@ minLineLength = 0
 maxLineGap = 400
 parallelism =[]
 lines = cv2.HoughLinesP(edges, rho=1 , theta= np.pi/180, threshold = 60, minLineLength=0, maxLineGap=400)
-#Checks for extra lines to ensure only parallels remain
+a = np.array(lines)
+print(a)
+#Checks for extra lines to ensure only parallels remain, does this by checking for repeating and unparallelled lines within a circle
 for x in range(len(lines)):
     for y in range(x+1, len(lines)):
         line1 = lines[x][0]
@@ -20,6 +22,7 @@ for x in range(len(lines)):
             parallelism.append((line1, line2))
             print(line1,line2)
 
+#essentially midpoint formula, finds the 4 points on the center line to draw it
 def drawing(inputImage, x1, y1, x2, y2, color, chungus):
     close = False
     for extras in parallelism:
@@ -31,7 +34,7 @@ def drawing(inputImage, x1, y1, x2, y2, color, chungus):
         color = (0,255, 0)
     cv2.line(inputImage, (x1, y1), (x2, y2), color, chungus)
 
-#Function will display the detected lines in image
+#Function will display the detected lines in image, in this case only midpoint
 for puntos in parallelism:
     drawing(inputImage, int((puntos[0][0] + puntos[1][0])/2), int((puntos[0][1]+puntos[1][1])/2), int((puntos[0][2]+ puntos[1][2])/2), int((puntos[0][3]+puntos[1][3])/2),(0, 0, 255), 2)
 
